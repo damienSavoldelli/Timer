4467ce7d651f7f36c61660c4bc3fefcc1d314332
damien <damien.savoldelli@gmail.com>
2019-08-02 01:12:04 +0200

    styling container & button Start

--- App/index.js
+++ App/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions} from 'react-native';
+
+const screen = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#07121B",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  button: {
+    borderWidth: 10,
+    borderColor: "#89AAFF",
+    width: screen.width / 2,
+    height: screen.width / 2,
+    borderRadius: screen.width / 2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontSize: 45,
+    color: "#89AAFF"
+  }
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <TouchableOpacity
+        onPress={() => alert("hello, world!")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 }
